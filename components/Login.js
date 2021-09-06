e9255cfd37a036c59d70483857cf81e70f6e7843
vclong2003 <vclong2003@gmail.com>
2021-09-06 23:00:27 +0700

    remove all firebase v9 code

--- components/Login.js
+++ components/Login.js
@@ -68,23 +68,6 @@ class Login {
             this.$inputGroupPassword.setError('Password must be greater 6 character')
         }
 
-        /* ----------------------------------------------(ver 9)
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user);
-            // ...
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
-          });
-          */
-
           firebase.auth().signInWithEmailAndPassword(email, password)
             .then((userCredential) => {
               // Signed in
--- index.js
+++ index.js
@@ -3,19 +3,6 @@ import { Hall } from './components/Hall.js'
 import { setScreen } from './setScreen.js'
 import { PlayNow } from './components/playNow.js';
 
-/* -----------------------------------------(firebase 9)
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        const hall = new Hall();
-        setScreen(hall);
-    } else {
-        const login = new Login();
-        setScreen(login)
-    }
-});
-*/
-
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       var uid = user.uid;
