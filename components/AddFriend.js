062e2f5152cb46e5066ce1c060e67714425868ca
huy1108 <quochuy11081999@gmail.com>
2021-09-13 21:10:21 +0700

    del friend, close formAddFriend

--- components/AddFriend.js
+++ components/AddFriend.js
@@ -1,29 +1,38 @@
 class AddFriend {
     $container
+    $listFriend
     $form
     $input
     $btn
+    listFriendItem
     constructor() {
         this.$container = document.createElement('div')
         this.$container.classList.add('addFriend')
 
+
+
         this.$listFriend = document.createElement('ul')
-        this.$listFriend.style.listStyle = 'none'
+        this.$listFriend.classList.add('list-friend')
+
 
 
         this.$form = document.createElement('form')
         this.$form.classList.add('addFriend-form')
         this.$form.addEventListener('submit', this.handleAddFriend)
 
+        this.$btnCloseAddFriend = document.createElement('button')
+        this.$btnCloseAddFriend.addEventListener('click', this.closeAddFriend)
+
+        this.$btnCloseAddFriend.classList.add('closeAddFriend-btn')
+        this.$btnCloseAddFriend.innerHTML = '<i class="fa fa-times-circle"></i>'
+
         this.$input = document.createElement('input')
         this.$input.classList.add('addFriend-input')
         this.$input.placeholder = 'Add friend here '
 
-        this.$btn = document.createElement('button')
-        this.$btn.classList.add('addFriend-btn')
-        this.$btn.innerHTML = '<i class="fa fa-plus-circle"></i>'
 
-        this.$listFriendItem = []
+
+        this.listFriendItem = []
 
         // firebase k support OR nen:
         firebase.auth().onAuthStateChanged((user) => {
@@ -43,10 +52,17 @@ class AddFriend {
             }
         });
 
+
+    }
+
+    closeAddFriend = () => {
+        this.setVisibleFriend(false)
+
     }
 
     handleAddFriend = (e) => {
         e.preventDefault()
+        if (this.$input.value.trim() == '') return
         db.collection('friends')
             .add({
                 a: firebase.auth().currentUser.email,
@@ -55,6 +71,9 @@ class AddFriend {
             .then(() => {
                 // this.setVisible(false)
             })
+
+        this.$input.value = ''
+
     }
 
 
@@ -62,6 +81,8 @@ class AddFriend {
         snapshot.docChanges().forEach((change) => {
 
             const friends = change.doc.data()
+            const id = change.doc.id
+            console.log(id);
             let friendEmail = ''
             if (firebase.auth().currentUser.email === friends.a) {
                 friendEmail = friends.b
@@ -70,20 +91,21 @@ class AddFriend {
             }
 
 
-
-            const $delFriend = document.createElement('button')
-            $delFriend.innerHTML = 'X'
-            $delFriend.classList.add('del-friend')
-
-
             const $li = document.createElement('li')
             $li.classList.add('list-friend-item')
+
             $li.dataset.email = friendEmail
+
             const $friend = document.createElement('div')
             $friend.classList.add('friend')
-            this.$listFriendItem.push($li)
+            this.listFriendItem.push($li)
 
-
+            const $delFriend = document.createElement('button')
+            $delFriend.addEventListener('click', () => {
+                this.handleDelFriend(friendEmail, $li, id)
+            })
+            $delFriend.innerHTML = 'X'
+            $delFriend.classList.add('del-friend')
 
 
             $friend.innerHTML = friendEmail
@@ -94,41 +116,60 @@ class AddFriend {
         })
     }
 
-    infoUserListener = (snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-            const infoUser = change.doc.data()
-            this.$listFriendItem.forEach((item) => {
-                if (item.dataset.email == infoUser.email) {
-                    const greenDot = document.createElement('div')
-                    greenDot.classList.add('green-dot')
-                    item.appendChild(greenDot)
+    handleDelFriend = (friendEmail, $li, id) => {
 
-
-                }
-            })
-
+        this.listFriendItem.forEach((listFriend) => {
+            if (friendEmail == listFriend.dataset.email) {
+                $li.remove()
+            }
         })
+
+        db.collection("friends").doc(id).delete()
+            .then(() => {
+            }).catch((error) => {
+                console.error("Error removing document: ", error);
+            });
+        // db.doc().update({ name: firebase.firestore.FieldValue.delete() })
+
     }
 
 
 
+    infoUserListener = (snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            const infoUser = change.doc.data()
 
+            if (infoUser.isOnl == true) {
+                this.listFriendItem.forEach((item) => {
+                    // console.log(infoUser.email);
+                    if (item.dataset.email == infoUser.email) {
+                        const greenDot = document.createElement('div')
+                        greenDot.classList.add('green-dot')
+                        item.appendChild(greenDot)
+
+                    }
+                })
+            }
+        })
+    }
 
 
     setVisibleFriend(visible) {
         if (visible) {
             this.$container.style.display = 'block'
+
+
         } else {
             this.$container.style.display = 'none'
+
 
         }
     }
 
 
-
     render() {
+        this.$container.appendChild(this.$btnCloseAddFriend)
         this.$form.appendChild(this.$input)
-        this.$form.appendChild(this.$btn)
         this.$container.appendChild(this.$form)
         this.$container.appendChild(this.$listFriend)
         return this.$container
